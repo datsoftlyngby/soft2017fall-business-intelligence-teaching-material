a69fdc1863c7c738a56c935208747f98dd28546e
Helge <rhp@cphbusiness.dk>
2017-07-31 16:29:56 +0200

    Assignment 1 for now, generation of plot is missing for the moment

---
+++ assignments/assignment_1/README.md
@@ -0,0 +1,21 @@
+# Assignment 1: Environment Setup and Introduction to Python
+
+Run the program `assignment_1.py` from **within your virtual machine**. Do neither run it in a local Python installation nor in a notebook on Azure!
+
+  1. Describe what files this program generates and attach the contents of each file together with its name to your solution.
+  2. What is the output of this program.
+  3. Describe in natural language and line-by-line what the program. Describe also for each line what the Python code expresses.
+
+
+## Hints:
+
+  * To run the program sucessfully you need to be connected to the internet.
+  * You can run the program either from the command-line `$ python assignment_1.py` or from within a notebook by importing `assignment_1.py` via:
+  ```
+  import assignment_1
+
+
+  assignment_1.run()
+  ```
+
+  To do the latter the file `assignment_1.py` has to be located in the same directory as the notebook you are working on.
---
+++ assignments/assignment_1/assignment_1.py
@@ -0,0 +1,90 @@
+import os
+import csv
+import requests
+import platform
+import statistics
+# import matplotlib.pyplot as plt
+
+
+def download_txt(url, save_path='./downloaded'):
+    response = requests.get(url)
+    with open(save_path, 'wb') as f:
+        f.write(response.content)
+
+
+def generate_csv(txt_input_path, csv_output_path):
+    with open(txt_input_path) as f:
+        txt_content = f.readlines()
+
+    rows = [['street', 'city', 'price', 'sqm', 'price_per_sqm']]
+    for line in txt_content:
+        line = line.rstrip().replace('  * ', '')
+        address, price, sqm = line.split('\t')
+        street, city = address.split('; ')
+        price_per_sqm = int(price) // int(sqm)
+        row = (street, city, price, sqm, price_per_sqm)
+        rows.append(row)
+
+    if platform.system() == 'Windows':
+        newline=''
+    else:
+        newline=None
+
+    with open(csv_output_path, 'w', newline=newline) as f:
+        output_writer = csv.writer(f)
+        for row in rows:
+            output_writer.writerow(row)
+
+
+def read_prices(csv_input_path):
+    with open(csv_input_path) as f:
+        reader = csv.reader(f)
+        _ = next(reader)
+
+        idxs = []
+        prices = []
+        for row in reader:
+            _, _, price, _, _ = row
+            idxs.append(reader.line_num)
+            prices.append(int(price))
+
+    return list(zip(idxs, prices))
+
+
+
+def compute_avg_price(data):
+    _, prices = zip(*data)
+    avg_price = statistics.mean(prices)
+
+    with open('/tmp/avg_price.txt', 'w') as f:
+        f.write(str(avg_price))
+
+    return avg_price
+
+
+def generate_plot(data):
+
+    x_values, y_values = data
+    plt.scatter(x_values, y_values, s=100)
+
+
+def run():
+    file_url = 'https://raw.githubusercontent.com/datsoftlyngby/' \
+               'soft2017fall-business-intelligence-teaching-material/master/' \
+               'assignments/assignment_1/price_list.txt'
+    txt_file_name = os.path.basename(file_url)
+    txt_path = os.path.join('./', txt_file_name)
+    download_txt(file_url, txt_path)
+    csv_file_name = 'price_list.csv'
+    csv_path = os.path.join(os.getcwd(), csv_file_name)
+    generate_csv(txt_path, csv_path)
+    data = read_prices(csv_path)
+    avg_price = compute_avg_price(data)
+    print(avg_price)
+    # genrate_plot(data)
+
+
+if __name__ == '__main__':
+    run()
+
+
